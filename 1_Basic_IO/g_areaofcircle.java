62653564694d1c4737806853906db9fb1519dfbb
mallabiju <mallabiju@gmail.com>
2023-05-12 07:24:57 +0545

    Area of circle

---
+++ 1_Basic_IO/g_areaofcircle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class g_areaofcircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter radius:");
+        float r = sc.nextFloat();
+        float area = (float) (3.14 * r * r);
+        System.out.println("Area of circle is:" + area);
+        sc.close();
+    }
+}
